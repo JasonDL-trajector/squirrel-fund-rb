b9dc497acb2523de8de4f8001e4bec88b173e6d8
JasonDL-trajector <emmanuel.delara@trajector.com>
2024-08-30 04:56:51 +0800

    fix dayJs

--- src/app/(DashboardLayout)/withdraw/page.tsx
+++ src/app/(DashboardLayout)/withdraw/page.tsx
@@ -16,7 +16,7 @@ import {
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import dayjs from 'dayjs';
+import dayjs, { Dayjs } from 'dayjs';
 import PageContainer from '@/app/(DashboardLayout)/components/container/PageContainer';
 
 const WithdrawPage = () => {

83c7f79760be80686b683be3efaacd91e28f5def
JasonDL-trajector <emmanuel.delara@trajector.com>
2024-08-30 03:50:13 +0800

    loading skeleton

--- src/app/(DashboardLayout)/loading.tsx
+++ src/app/(DashboardLayout)/loading.tsx
@@ -1,7 +1,18 @@
-const Loading = () =>{
-    return(
-        <div>Dash Loading</div>
-    )
-}
+import React from 'react';
+import { Box, Skeleton } from '@mui/material';
+
+const Loading = () => {
+  return (
+    <Box sx={{ p: 3 }}>
+      <Skeleton variant="rectangular" width="100%" height={200} sx={{ mb: 2 }} />
+      <Skeleton variant="text" width="60%" height={40} sx={{ mb: 2 }} />
+      <Skeleton variant="rectangular" width="100%" height={100} sx={{ mb: 2 }} />
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Skeleton variant="rectangular" width="48%" height={150} />
+        <Skeleton variant="rectangular" width="48%" height={150} />
+      </Box>
+    </Box>
+  );
+};
 
 export default Loading;
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,17 @@
-const Loading = () =>{
-    return(
-        <div>Loading</div>
-    )
-}
+import React from 'react';
+import { Box, Skeleton } from '@mui/material';
+
+const Loading = () => {
+  return (
+    <Box sx={{ p: 3, maxWidth: 400, mx: 'auto' }}>
+      <Skeleton variant="circular" width={80} height={80} sx={{ mx: 'auto', mb: 2 }} />
+      <Skeleton variant="text" width="80%" height={40} sx={{ mx: 'auto', mb: 2 }} />
+      <Skeleton variant="rectangular" width="100%" height={56} sx={{ mb: 2 }} />
+      <Skeleton variant="rectangular" width="100%" height={56} sx={{ mb: 2 }} />
+      <Skeleton variant="rectangular" width="100%" height={56} sx={{ mb: 2 }} />
+      <Skeleton variant="rectangular" width="100%" height={40} />
+    </Box>
+  );
+};
 
 export default Loading;

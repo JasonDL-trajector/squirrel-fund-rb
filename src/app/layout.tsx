48d3efabeced287e2538d2c6e1d7b048be35afa8
JasonDL-trajector <emmanuel.delara@trajector.com>
2024-08-30 20:13:16 +0800

    fix createTheme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,28 @@
 import { Metadata } from 'next';
-import { baselightTheme } from '@/utils/theme/DefaultColors';
-import { ThemeProvider } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
+import ClientThemeProvider from '@/components/ClientThemeProvider';
 
-// export const metadata: Metadata = {
-//   title: 'Squirrel Fund',
-//   description: 'Manage your finances with Squirrel Fund',
-//   manifest: '/manifest.json',
-//   themeColor: '#1565c0',
-//   appleWebApp: {
-//     capable: true,
-//     statusBarStyle: 'default',
-//     title: 'Squirrel Fund',
-//   },
-//   viewport: {
-//     width: 'device-width',
-//     initialScale: 1,
-//     maximumScale: 1,
-//   },
-// }
+export const metadata: Metadata = {
+  title: 'Squirrel Fund',
+  description: 'Manage your finances with Squirrel Fund',
+  manifest: '/manifest.json',
+  themeColor: '#1565c0',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: 'Squirrel Fund',
+  },
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+  },
+}
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-
   return (
     <html lang="en">
       <head>
@@ -35,13 +32,9 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-title" content="Squirrel Fund" />
       </head>
       <body>
-      <ThemeProvider theme={baselightTheme}>
-      <CssBaseline />
-         
-            {children}
-          
-      </ThemeProvider>
-      
+        <ClientThemeProvider>
+          {children}
+        </ClientThemeProvider>
       </body>
     </html>
   );
